880011987b72ba15ccb492d6b3d7b65bb330554e
Phan Tran Thanh Huy <alexspector8766@gmail.com>
2025-07-09 17:40:54 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,7 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-  output: 'export', // rất quan trọng để dùng `next export`
-  basePath: isProd ? '/hitori.github.io' : '',
+  basePath: '/hitori.github.io',
   assetPrefix: isProd ? '/hitori.github.io/' : '',
   images: {
     unoptimized: true, // vì Next export không hỗ trợ Image Optimization
