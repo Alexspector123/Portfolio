a57a72aa13a1ca043763a3cf7d7fc971192d3331
Phan Tran Thanh Huy <alexspector8766@gmail.com>
2025-07-09 16:02:01 +0700

    custome next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // Quan trọng để static export
+  distDir: 'out', // Thư mục xuất ra sau khi build
+  images: {
+    unoptimized: true, // Bắt buộc khi dùng `output: export`
+  },
+  basePath: '', // Với github.io repo chính thì để rỗng
 };
 
 export default nextConfig;
